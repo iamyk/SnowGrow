0c7e0de9feb0925db1c27b33e8803498fd7ca614
Amy Kiernan <theamykins@gmail.com>
2022-08-20 23:40:53 -0500

    Initial Commit

---
+++ SnowGrow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowGrow
+//
+//  Created by Logan Monaco on 8/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnowGrow/SnowGrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowGrowApp.swift
+//  SnowGrow
+//
+//  Created by Logan Monaco on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowGrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
